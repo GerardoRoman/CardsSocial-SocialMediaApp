8447e31b26a1548e08b531893bb4db5a574092c4
GerardoRoman <offthegrd3657@gmail.com>
2023-03-29 16:00:19 -0400

    added navbar

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,21 +1,28 @@
-import { FaBars, FaTimes } from 'react-icons/fa'
-import { useRef } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { useState } from 'react'
+import "../styles/navbar.css"
 
 function Navbar() {
+    const [expanded, setExpanded] = useState(false)
+
     return ( 
-        <header>
+        <header className='header'>
             <h3>Testing</h3>
             <nav>
-                <a href="/#">Home</a>
-                <a href="/#">My Profile</a>
-                <a href="/#">Logout</a>
-                <button>
-                    <FaTimes />
-                </button>
+                <a href="/home">Home</a>
+                <a href="/my_profile">My Profile</a>
+                <a href="/new_card">Make a new card</a>
+                <a href="/logout">Logout</a>
+                <div className='hamburger'>
+                {expanded && <a href="home">Home</a>}
+                {expanded && <a href="/my_profile">My Profile</a>}
+                {expanded && <a href="/new_card">Make a new card</a>}
+                {expanded && <a href="/logout">Logout</a>}
+                    <button className='nav-button' onClick={() => setExpanded(!expanded)} aria-expanded={expanded}>
+                        <FaBars />
+                    </button>
+                </div>
             </nav>
-            <buttons>
-                <FaBars />
-            </buttons>
         </header>
     )
 }
