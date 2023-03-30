1e83b29fa7158d02c926ab3bd82f1a461d051df0
MichaelGreason <mgreason18@gmail.com>
2023-03-30 14:59:14 -0400

    fixed navbar back into app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Cards from './cards.js'
 import CardList from './cardList.js'
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
       <Cards />
       <CardList />
     </>
