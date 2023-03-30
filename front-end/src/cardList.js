71a8727bb65cca4a6ccea6de89c0f8f7607aef73
esantb <e.saint.bou@gmail.com>
2023-03-30 13:33:21 -0500

    cards mapping correctly, profiles are working bare bones

--- front-end/src/cardList.js
+++ front-end/src/cardList.js
@@ -3,8 +3,9 @@ import { useState } from 'react';
 import Cards from "./cards";
 
 
-export default function CardList(props) {
+export default function CardList() {
     const cardArray = ["Card 1", "Card 2", "Card 3"]
+    // const []
 
 
 
