3b0f9e9cbb18985ec6a2a976c8b25217ea869f73
GerardoRoman <offthegrd3657@gmail.com>
2023-03-30 14:19:35 -0400

    finished basic navbar functionality

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -8,17 +8,17 @@ function Navbar() {
     return ( 
         <header className='header'>
             <h3>Testing</h3>
-            <nav>
-                <a href="/home">Home</a>
-                <a href="/my_profile">My Profile</a>
-                <a href="/new_card">Make a new card</a>
-                <a href="/logout">Logout</a>
+            <nav className='navbar'>
                 <div className='hamburger'>
-                {expanded && <a href="home">Home</a>}
-                {expanded && <a href="/my_profile">My Profile</a>}
-                {expanded && <a href="/new_card">Make a new card</a>}
-                {expanded && <a href="/logout">Logout</a>}
-                    <button className='nav-button' onClick={() => setExpanded(!expanded)} aria-expanded={expanded}>
+                {expanded && <div>
+                    <li>
+                        <a href="/home">Home</a>
+                        </li> 
+                        <li><a href="/my_profile">My Profile</a></li> 
+                        <li><a href="/new_card">Make a new card</a></li>
+                        <li><a href="/logout">Logout</a></li> 
+                        </div>}
+                    <button className='hamburger' onClick={() => setExpanded(!expanded)} aria-expanded={expanded}>
                         <FaBars />
                     </button>
                 </div>
@@ -28,3 +28,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
