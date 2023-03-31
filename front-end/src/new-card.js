651414e9943637626f4a8a47ca659ab808a92fed
MichaelGreason <mgreason18@gmail.com>
2023-03-31 13:37:44 -0400

    all customization works, still need to add a few UI clarifiaction text and still need to create the axios push request.

--- front-end/src/new-card.js
+++ front-end/src/new-card.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import styled, { css } from 'styled-components'
+import { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components'
 
 
 function NewCard(){
-    const [background, setBackground] = useState('')
-    const [border, setBorder] = useState('')
-    const [font, setFont] = useState(null)
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const colorChoices = ['Blue', 'Red', 'Green']
-    const borderChoices = ['Dotted', 'Dashed', 'Solid']
-    const fontChoices = ['Handwritten', 'Plain', 'Cursive']
+    const [background, setBackground] = useState('');
+    const [border, setBorder] = useState('');
+    const [font, setFont] = useState(null);
+    const Title = useRef('')
+    const Body = useRef('');
+    const Back = useRef('')
+    const colorChoices = ['Blue', 'Red', 'Green'];
+    const borderChoices = ['Dotted', 'Dashed', 'Solid'];
+    const fontChoices = ['Handwritten', 'Plain', 'Cursive'];
+    const [resultsObject, setResultsObject] = useState([])
 
     const StyledTextArea = styled.textarea`
         background-color: rgba(0, 0, 0, 0);
         border-color: rgba(0, 0, 0, 0);
         overflow: auto;
         outline: none;
-        // border: 1px solid blackl;
+        // border: 1px solid black;
         width: 75%;
         height: 23rem;
         rows: "33";
@@ -26,6 +28,38 @@ function NewCard(){
         resize: none;
         font-family: ${font};
         font-size: 30px;
+    `
+    const StyledBackArea = styled.textarea`
+        background-color: rgba(0, 0, 0, 0);
+        border-color: rgba(0, 0, 0, 0);
+        outline: none;
+        // border: 1px solid black;
+        width: 75%;
+        height: 23rem;
+        rows: "33";
+        cols: "50";
+        resize: none;
+        font-family: ${font};
+        font-size: 30px;
+        text-align: center;
+        margin-top: 30%        
+    `
+
+
+    const TitleBox = styled.input`
+    background-color: rgba(0, 0, 0, 0);
+    border-color: rgba(0, 0, 0, 0);
+    overflow: auto;
+    outline: none;
+    // border: 1px solid black;
+    width: 100%;
+    height: 3rem;
+    font-family: ${font};
+    font-size: 30px;
+    resize: none;
+    text-align: center;
+    // border-bottom: 2px solid black
+    margin-top: 3px;
     `
 
     const BackgroundColor = styled.section`
@@ -54,7 +88,23 @@ function NewCard(){
     console.log('Background Color: ', background);
     console.log('Border Selection: ', border);
     console.log('Font Selection: ', font);
+    console.log('Title Text: ', Title.current.value)
+    console.log('Body Text: ', Body.current.value);
+    console.log('Back Text: ', Back.current.value);
+    setResultsObject({
+        color: background,
+        border: border,
+        font: font,
+        title_text: Title.current.value,
+        card_front_message: Body.current.value,
+        card_back_message: Back.current.value,
+    })
     }
+
+    console.log(resultsObject)
+
+    
+
 
     function backgroundChoice(colors) {
         console.log('clicked')
@@ -95,59 +145,87 @@ function NewCard(){
         }
     }
 
-    const handleBodyChange = event => {
-        setBody(event.target.value);
-        console.log(body);
-    }
-
     return (
         <>
         <h1>New Card</h1>
-        <br></br>
+        <br/>
+        <div className='customize-buttons'>
+            <br/>
+        <span>
+        {colorChoices.map(colors => <button className='ind-but' onClick={backgroundChoice}>{colors}</button>)}
+        </span>
+        <br/>
+        <span>
+            {borderChoices.map(borders => <button className='ind-but' onClick={borderChoice}>{borders}</button>)}
+        </span>
+        <br/>
+        <span>
+            {fontChoices.map(fonts => <button className='ind-but' onClick={fontChoice}>{fonts}</button>)}
+        </span>   
         
-        <span>
-        {colorChoices.map(colors => <button className='color-buttons' onClick={backgroundChoice}>{colors}</button>)}
-        </span>
-        <span>
-            {borderChoices.map(borders => <button className='border-buttons' onClick={borderChoice}>{borders}</button>)}
-        </span>
-        <span>
-            {fontChoices.map(fonts => <button className='font-buttons'onClick={fontChoice}>{fonts}</button>)}
-        </span>
-        <div className='card'>
-        <BackgroundColor> 
-            <BorderChoice>  
-                <FontChoice>      
-        <div> 
-            <div className='title'>Example Title</div>
-                <div className='card-body'>
-                    <StyledTextArea placeholder='Roses are red' id='body-text' value={body} onChange={handleBodyChange}></StyledTextArea>              
+        </div>
+        <br />
+        <div className='card-container'>
+            <div className='card'>
+            <BackgroundColor> 
+                <BorderChoice>  
+                    <FontChoice>      
+            <div> 
+                <TitleBox placeholder='Title' id='title' name='title' ref={Title}></TitleBox>
+                    <div className='card-body'>
+                        <StyledTextArea placeholder='Roses are red' id='body' name='body' ref={Body}></StyledTextArea>              
+                </div>
+            </div>
+                    </FontChoice> 
+                </BorderChoice>
+            </BackgroundColor>
+            </div>
+
+            <div className='card'>
+            <BackgroundColor> 
+                <BorderChoice>  
+                    <FontChoice>      
+            <div> 
+                    <div className='card-back'>
+                        <StyledBackArea placeholder='Sign here' id='back' name='back' ref={Back}></StyledBackArea>              
+                </div>
+            </div>
+                    </FontChoice> 
+                </BorderChoice>
+            </BackgroundColor>
             </div>
         </div>
-                </FontChoice> 
-            </BorderChoice>
-        </BackgroundColor>
-        </div>
+
         <br></br>
         
         <form onSubmit={handleSubmit}>
             <div>
-            <label>Color Choice: </label>
-                <input value={background} readOnly={true}></input>
+            <label></label>
+                <input value={background} readOnly={true} hidden={true}></input>
             </div>
             <div>
-            <label>Border Choice: </label>
-                <input value={border} readOnly={true}></input>
+            <label></label>
+                <input value={border} readOnly={true} hidden={true}></input>
             </div>
             <div>
-            <label>Font Choice: </label>
-                <input value={font} readOnly={true}></input>
+            <label></label>
+                <input value={font} readOnly={true} hidden={true}></input>
             </div>
             <div>
-                <label for='body-text'>Body Text: </label>
-                <input></input>
+                <label for='body-text'></label>
+                <input value={Body.current} hidden={true}></input>
             </div>
-            <button type='submit'>Submit</button>
+            <div>
+                <label for='title-text'></label>
+                <input value={Title.current} hidden={true}></input>
+            </div>
+            <div>
+            <label for='back-text'></label>
+                <input value={Back.current} hidden ></input>
+            </div>
+            <div className='submit'>
+            <button type='submit' readOnly={true}>Submit</button>
+            </div>
         </form>
         
         </>
