26791b28fef91d176d62cb80ec9ea0f3c19da7e9
esantb <e.saint.bou@gmail.com>
2023-04-06 09:19:41 -0500

    password disclaimer works

--- front-end/src/components/followUnfollowButton.js
+++ front-end/src/components/followUnfollowButton.js
@@ -1,42 +1,42 @@
-// import { useState } from 'react'
-// import axios from 'axios'
-// import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-// function FollowUnfollowButton({ token }) {
-//     const [userName, setUserName] = useState('')
-//     const [follow, setFollow] = useState('')
-//     const [unfollow, setUnfollow] = useState('')
+function FollowUnfollowButton({ token }) {
+    const [userName, setUserName] = useState('')
+    const [follow, setFollow] = useState('')
+    const [unfollow, setUnfollow] = useState('')
 
-//     const handleFollow = (event) => {
-//         event.preventDefault()
-//         axios.post('https://social-cards-app.onrender.com/follow/username/', {
-//             headers: {
-//                 Authorization: `Token ${token}`
-//             }
-//         }).then(res => {
-//             setFollow(res.data.results);
-//             console.log(res.data.results);
-//         })
-//     };
+    const handleFollow = (event) => {
+        event.preventDefault()
+        axios.post('https://social-cards-app.onrender.com/follow/username/', {
+            headers: {
+                Authorization: `Token ${token}`
+            }
+        }).then(res => {
+            setFollow(res.data.results);
+            console.log(res.data.results);
+        })
+    };
 
-//     const handleUnfollow = (event) => {
-//         event.preventDefault()
-//         axios.delete('https://social-cards-app.onrender.com/follow/username/', {
-//             headers: {
-//                 Authorization: `Token ${token}`
-//             }
-//         }).then(res => {
-//             setUnfollow(res.data.results);
-//             console.log(res.data.results);
-//         })
-//     };
-
-
-//     return (
-//         <button onClick={handleFollow}>Follow!</button>
+    const handleUnfollow = (event) => {
+        event.preventDefault()
+        axios.delete('https://social-cards-app.onrender.com/follow/username/', {
+            headers: {
+                Authorization: `Token ${token}`
+            }
+        }).then(res => {
+            setUnfollow(res.data.results);
+            console.log(res.data.results);
+        })
+    };
 
 
-//     )
-// }
+    return (
+        <button onClick={handleFollow}>Follow!</button>
 
-// export default FollowUnfollowButton();
+
+    )
+}
+
+export default FollowUnfollowButton();
